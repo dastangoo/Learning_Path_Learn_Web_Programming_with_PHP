f2755c7c245890dc637a9f6d3f58111bc6a10995
dastangoo <hamed.dastangoo@gmail.com>
2017-05-26 09:29:44 +0430

    Basic Introduction to Variables

---
+++ Examples/basic_variables_1.php
@@ -0,0 +1,17 @@
+<?php
+$company = 'ABC Consulting';
+
+$name = 'VALUE';
+$_another = 22;
+$name2 = 'OTHER';
+
+$a = 1;
+$b = 'Some Text';
+
+$firstName = 'Doug';
+$cityStatePostalCode = 'New York, New York 10001';
+$hourlyRate = 47.22;
+
+$thisIsARidiculousVariableName = 'test';
+echo $thisIsARidiculousVariableName;
+ ?>
---
+++ Examples/basic_variables_2.php
@@ -0,0 +1,17 @@
+<?php
+$company = 'ABC Consulting';
+$link1 = 'About Us';
+$link2 = 'Products';
+$link3 = 'Services';
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title><?php echo $company; ?></title>
+   </head>
+   <body>
+
+   </body>
+ </html>
