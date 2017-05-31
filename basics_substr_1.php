8954d0a4f54dccb6c7cca280fabb375b06c3de0c
dastangoo <hamed.dastangoo@gmail.com>
2017-05-31 20:18:36 +0430

    substr() function

---
+++ basics_substr_1.php
@@ -0,0 +1,13 @@
+<?php
+// Understanding the Basics: Understanding the substr() command
+// first part of a string = start with 0
+
+// syntax substr($string, $start, $length)
+$firstName = 'Hamed';
+$lastName = 'Dastangoo';
+
+// print the name
+printf('<br />Full Name: %s %s', $firstName, $lastName);
+// use substr to get the 1st Initials
+// NOTE: strings start with 0, not 1!
+printf('<br />Initials: %s%s', substr($firstName, 0, 1), substr($lastName, 0, 1));
---
+++ basics_substr_2.php
@@ -0,0 +1,16 @@
+<?php
+// Understanding the Basics: Understanding the substr() command
+// middle part of a stirng: start > 0
+
+// syntax substr($string, $start, $length)
+$name = 'Mr. George Clooney';
+printf('<br />Formal Name: %s', $name);
+
+// pull out the first name
+printf('<br />First Name: %s', substr($name, 4, 6));
+
+// leave off the length and the reminder is returned
+printf('<br />Name without Title: %s', substr($name, 4));
+
+// NOTE: if you don't know the exact position, you can
+//       combine substr() with strpos() (discussed in another lesson)
