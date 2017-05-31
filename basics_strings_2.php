c3f1ef0763811a93eba62752df373dcf139625ba
dastangoo <hamed.dastangoo@gmail.com>
2017-05-31 22:00:47 +0430

    Strings

--- basics_strings_2.php
+++ basics_strings_2.php
@@ -1,8 +1,20 @@
 <?php
+// Understanding the Basics: Working with Strings
+// trim() removes space from *both* beginning and end of a string
+
+// assing a value
 $untrimmedstring = ' This is a string with spaces on the end.    ';
+
+// use strlen() to get the length of the untrimmed string
 $lengthUntrimmed = strlen($untrimmedstring);
+
+// now apply trim()
 $trimmedString = trim($untrimmedstring);
+
+// get the length of the trimmed string
 $lengthTrimmed = strlen($trimmedString);
+
+// dispaly the results:
 printf('<br />String Before --->|%s|<---', $untrimmedstring);
 printf('<br />String After --->|%s|<---', $trimmedString);
 printf('<br />Length Before ---> %d', $lengthUntrimmed);
