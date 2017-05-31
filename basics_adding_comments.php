f61ba594c12f02c389f7946e029ccaaded3a4fdb
dastangoo <hamed.dastangoo@gmail.com>
2017-05-31 18:52:21 +0430

    Adding Comments

---
+++ basics_adding_comments.php
@@ -0,0 +1,20 @@
+<?php
+// Understanding the Basics: Adding Comments
+
+// This is a single line comment
+echo "<br />The single line comment does not appear, but this text does!";
+
+# Comments do not appear when viewing the page source
+echo "<br />Comments do not appear when viewing the page source";
+
+$value = 'inline comment';      // This is an inline comment
+$value2 = 'another inline comment'; # This is another comment
+echo "<br />The $value does not appear, but this text does!";
+
+
+/*
+ * This is a
+ * multilne
+ * comment
+ */
+ echo "<br />The multiline comment does not appear, but this text does!";
