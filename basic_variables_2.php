7a29fd581f1af2c706bbe720ecad52c749618618
dastangoo <hamed.dastangoo@gmail.com>
2017-05-31 17:37:43 +0430

    Variables Introduction

---
+++ basic_variables_2.php
@@ -0,0 +1,20 @@
+<?php
+// Understanging the Basics: Creating Variables in PHP
+// variables are used to store values
+
+// changing the value of the variable can make instant changes to a website
+$company = 'My Own Company';
+$link1 = 'About Us';
+$link2 = 'Products';
+$link3 = 'Services';
+?>
+
+<!DOCTYPE html>
+<head>
+  <!-- displays the value to the browser -->
+  <title><?php echo $company; ?></title>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+<body>
+
+</body>
