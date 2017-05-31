75200ade563032e432fff25b3924f4d9abd5d438
dastangoo <hamed.dastangoo@gmail.com>
2017-05-31 19:30:29 +0430

    Constants

---
+++ basics_constant_values_2.php
@@ -0,0 +1,26 @@
+<?php
+// Understanding the Basics: Assigning Cosntant Values
+
+// predefined constants are assigned when PHP is installed
+// some examples:
+
+// version of PHP
+echo '<br />Version: ';
+echo PHP_VERSION;
+
+// translates to newline on Linux and Mac, carriage return/line feed on Windows
+// NOTE: this is only visible when you view the page source
+// In Internet Explorer and Firefox: View - Page / Source
+// ON a Mac: hold down CTRL, click, and select "View Source"
+
+echo '<br /> End of Line: ';
+echo PHP_EOL;
+
+// maximum size of an integer
+echo '<br />Max Int: ';
+echo PHP_INT_MAX;
+echo PHP_EOL;
+
+// for more info: http://php.net/manual/en/reserved.constants.php
+$reference = 'http://php.net/manaual/en/reserved.constants.php';
+echo "<br /><a href='$reference'>More Info</a>";
