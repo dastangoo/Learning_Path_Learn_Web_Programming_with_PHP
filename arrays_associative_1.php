737cd3eb618989d24589bf272d4528a670783457
dastangoo <hamed.dastangoo@gmail.com>
2017-06-01 09:29:06 +0430

    Associative Array

---
+++ arrays_associative_1.php
@@ -0,0 +1,9 @@
+<?php
+$name['first'] = 'Julia';
+$name['last'] = 'Roberts';
+$name['age'] = 37;
+$name['occupation'] = 'Actress';
+
+var_dump($name);
+
+printf('<br />Full Name: %s %s', $name['first'], $name['last']);
