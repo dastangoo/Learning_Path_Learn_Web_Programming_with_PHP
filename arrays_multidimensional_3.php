d5bff96aee48a207ab7f6b76298ec818f8308b0f
dastangoo <hamed.dastangoo@gmail.com>
2017-06-01 10:32:35 +0430

    Multidimensional Array

---
+++ arrays_multidimensional_3.php
@@ -0,0 +1,13 @@
+<?php
+// Working with Arrays: Single Assignment
+// echo $array[key][key]
+
+
+echo '<pre>';
+// create a 3 x 3 matrix
+$matrix = array(array(1, 2, 3), array(4, 5, 6), array(7, 8, 9));
+
+var_dump($matrix);
+
+echo '</pre>';
+echo "<br />Middle Element: {$matrix[1][1]}";
