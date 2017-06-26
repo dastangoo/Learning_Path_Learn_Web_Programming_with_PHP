a7f6480439e7cd7a647c753e431ccf8ba8e94c20
dastangoo <hamed.dastangoo@gmail.com>
2017-06-26 10:30:03 +0430

    Using include statements

---
+++ external_include_1.php
@@ -0,0 +1,34 @@
+<!--  Managing the Code -- External Libraries -->
+<!--  use include when you want to load a file one or more times -->
+<?php  include './includes/constants.php'; ?>
+<div style="width: 635px; height: 6500x;">
+  <h1><?php echo SITE_HEADER; ?></h1>
+  <div style="width: 110px; height: 600px; float: left;">
+    <?php include './includes/ad_block.html' ?>
+    <?php include './includes/ad_block.html' ?>
+  </div>
+  <div style="border: solid blue 1px; width: 500px; height: 410px; float: left; font: 12px arial,sans-serif; padding: 5px">
+      <h3><?php echo CONTENT_HEADER; ?></h3>
+      <p>
+        Lorem ipsum dolor sit amet, simul animal nostrud cu qui, eu sed aliquid blandit nominati, ea eum nulla congue. Cu quidam electram patrioque vel, decore ubique insolens per ex. Sit sonet volumus ea, ad modo corpora mea. Ne sea mucius recusabo.
+        Vis elit prodesset te, cu eos aperiam euripidis, alii natum repudiare est cu. Ullum nihil per id. Ex duo alterum liberavisse suscipiantur. An hinc accommodare mediocritatem mea, et lorem consul recteque sea. Te cum ridens voluptaria, essent cetero per et.
+        Ne sit integre ancillae, ei alienum singulis his. Alterum dissentiunt duo eu, mei at aeterno perpetua. Ut quando atomorum per, iriure sanctus maiestatis duo ei, ex vis summo gubergren. Vel nusquam efficiantur ut, nostro instructior his at, ei ridens ceteros salutatus duo. Vim feugiat delectus incorrupte id. An quo doctus tractatos.
+        Eum nobis dictas no, ne quas putant officiis ius. Ius stet suavitate dissentiet ad, scaevola mnesarchum ut eum, in duis error dissentias usu. Essent alterum eu per. Ad cetero consetetur vix, sed aperiri adipisci lobortis ea.
+        Affert albucius cu nec. Eos cu impedit voluptua pericula. Sea et graece offendit, erat possit tamquam ei nec. Pro possit admodum suscipit ea, utamur mnesarchum ea eum. Eam soleat lucilius id, ut vel efficiendi interesset, feugait omnesque mel eu. Ut tale elitr eruditi eum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, simul animal nostrud cu qui, eu sed aliquid blandit nominati, ea eum nulla congue. Cu quidam electram patrioque vel, decore ubique insolens per ex. Sit sonet volumus ea, ad modo corpora mea. Ne sea mucius recusabo.
+        Vis elit prodesset te, cu eos aperiam euripidis, alii natum repudiare est cu. Ullum nihil per id. Ex duo alterum liberavisse suscipiantur. An hinc accommodare mediocritatem mea, et lorem consul recteque sea. Te cum ridens voluptaria, essent cetero per et.
+        Ne sit integre ancillae, ei alienum singulis his. Alterum dissentiunt duo eu, mei at aeterno perpetua. Ut quando atomorum per, iriure sanctus maiestatis duo ei, ex vis summo gubergren. Vel nusquam efficiantur ut, nostro instructior his at, ei ridens ceteros salutatus duo. Vim feugiat delectus incorrupte id. An quo doctus tractatos.
+        Eum nobis dictas no, ne quas putant officiis ius. Ius stet suavitate dissentiet ad, scaevola mnesarchum ut eum, in duis error dissentias usu. Essent alterum eu per. Ad cetero consetetur vix, sed aperiri adipisci lobortis ea.
+        Affert albucius cu nec. Eos cu impedit voluptua pericula. Sea et graece offendit, erat possit tamquam ei nec. Pro possit admodum suscipit ea, utamur mnesarchum ea eum. Eam soleat lucilius id, ut vel efficiendi interesset, feugait omnesque mel eu. Ut tale elitr eruditi eum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, simul animal nostrud cu qui, eu sed aliquid blandit nominati, ea eum nulla congue. Cu quidam electram patrioque vel, decore ubique insolens per ex. Sit sonet volumus ea, ad modo corpora mea. Ne sea mucius recusabo.
+        Vis elit prodesset te, cu eos aperiam euripidis, alii natum repudiare est cu. Ullum nihil per id. Ex duo alterum liberavisse suscipiantur. An hinc accommodare mediocritatem mea, et lorem consul recteque sea. Te cum ridens voluptaria, essent cetero per et.
+        Ne sit integre ancillae, ei alienum singulis his. Alterum dissentiunt duo eu, mei at aeterno perpetua. Ut quando atomorum per, iriure sanctus maiestatis duo ei, ex vis summo gubergren. Vel nusquam efficiantur ut, nostro instructior his at, ei ridens ceteros salutatus duo. Vim feugiat delectus incorrupte id. An quo doctus tractatos.
+        Eum nobis dictas no, ne quas putant officiis ius. Ius stet suavitate dissentiet ad, scaevola mnesarchum ut eum, in duis error dissentias usu. Essent alterum eu per. Ad cetero consetetur vix, sed aperiri adipisci lobortis ea.
+        Affert albucius cu nec. Eos cu impedit voluptua pericula. Sea et graece offendit, erat possit tamquam ei nec. Pro possit admodum suscipit ea, utamur mnesarchum ea eum. Eam soleat lucilius id, ut vel efficiendi interesset, feugait omnesque mel eu. Ut tale elitr eruditi eum.
+      </p>
+  </div>
+</div>
