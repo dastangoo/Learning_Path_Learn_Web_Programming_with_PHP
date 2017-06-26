3fe131fd84b377fb5cda037f35f04e787b8a8a7b
dastangoo <hamed.dastangoo@gmail.com>
2017-06-26 18:27:52 +0430

    Using file family commands

---
+++ files_file_family_3.php
@@ -0,0 +1,10 @@
+<?php
+// Managing Files: Using the "*file" family
+// using "f*" family commands to write to a file
+
+// filename
+$filename = 'website_access.log';
+
+// file() command returing an array
+$entries = file($filename);
+echo '<pre>' . var_dump($entries) . '</pre>';
