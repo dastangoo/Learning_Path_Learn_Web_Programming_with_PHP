9612c889355c5cec3fa4e352e52c8b5941522ce8
dastangoo <hamed.dastangoo@gmail.com>
2017-07-19 19:42:09 +0430

    Capturing Form Data

---
+++ web-forms_capturing_2.php
@@ -0,0 +1,33 @@
+<?php
+// Building Web Forms: Capturing <form> Data
+
+// capturing any input data
+if (isset($_POST['submit'])) {
+  // captures data from web-forms_capturing_1.html
+  $data = var_export($_POST, TRUE) . PHP_EOL;
+}
+else {
+  $data = '';
+}
+
+// read form into a variable
+$form = file_get_contents('web-forms_capturing_2.html');
+
+// replace action="#ACTION#" using str_ireplace()
+$form = str_ireplace('#DATA#', htmlspecialchars($data), $form);
+
+// echo header HTML
+echo '<!DOCTYPE html>';
+echo '<!-- Building Web Forms: Capturing <form> Data -->';
+echo '<head>';
+echo '<meta charset="utf-8">';
+echo '<title>Building Web Forms</title>';
+echo '<link href="css/style.css" media="screen" rel="stylesheet" type="text/css" >';
+echo '<link href="images/favicon.icon?v=2" rel="shortcut icon" type="image/vnd.microsoft.icon" >';
+
+// echo form
+echo $form;
+
+// echo closing HTML
+echo '</body>';
+echo '</html>';
---
+++ web-forms_capturing_3.php
@@ -0,0 +1,45 @@
+<?php
+// Building Web Forms: Capturing <form> Data
+// using a class to build the form
+// NOTE: data comes back in the form of an array $_POST['data']['key']
+
+// include the "Form" class defintion file
+ini_set('display_errors', 1);
+require 'web-forms_capturing_3_class.php';
+
+// init variables
+$header = 'Capturing Form Data';
+$title = 'Building Web Forms';
+
+// check to see if there's any input
+if (isset($_POST['data'])) {
+  // because all form elements have name = data(key)
+  // we can pull all data from the one key in $_POST
+  // NOTE: use var_export($data, TRUE) to produce output of var_dump() without directly outputing right here!
+  $postData = var_export($_POST['data'], TRUE);
+}
+else {
+  $postData = '';
+}
+
+// load class
+$form = new Form();
+
+// build form contents
+$formContents = $form->input('ID', 'text', 'id', 6, 8);
+$formContents .= $form->input('Name', 'text', 'name', 60, 40);
+$formContents .= $form->textarea('Address', 'address', 4, 40);
+$formContents .= $form->input('City', 'text', 'city', 50, 255);
+$formContents .= $form->input('Postal Code', 'text', 'postcode', 16, 16);
+$formContents .= $form->input('Phone', 'text', 'phone', 20, 20);
+$formContents .= $form->submit('submit', 'Update');
+
+// put form contents into form tag
+$formHeader = $form->h(4, $header);
+$formTable .= $form->buildForm('userInfo', $_SE RVER['REQUEST_URI'], $form->div('aTable', $formContents));
+$formDiv .= $form->div('theForm', $formHeader . $formTable);
+$formDiv .= $form->div('data', $postData);
+$formFooter = $form->footer('&copy; 2012 by InfiniteSkills.com | All rights reserved');
+
+// echo HTML
+echo $form->html($title, $form->div('container', $formDiv . $dataDiv . $formFooter));
