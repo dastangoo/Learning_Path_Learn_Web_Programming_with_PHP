231abaf8b892fc5103c0f709082e21fdde44d238
dastangoo <hamed.dastangoo@gmail.com>
2017-07-20 11:45:11 +0430

    Input Filtering

---
+++ test-00.php
@@ -0,0 +1,8 @@
+<?php
+$clean_data = filter_input_array(INPUT_POST, array(
+  // each field in turn, with desired defintion
+  'name' => FILTER_SANITIZES_STRING,
+  'email' => FILTER_VALIDATE_EMAIL
+));
+
+var_dump($clean_data);
