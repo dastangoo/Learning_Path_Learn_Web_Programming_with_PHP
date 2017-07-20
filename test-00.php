94a08db38e348c33dc00c1afbeb11f3289343ff2
dastangoo <hamed.dastangoo@gmail.com>
2017-07-20 11:33:00 +0430

    Generators

---
+++ test-00.php
@@ -0,0 +1,13 @@
+<?php
+function getValues()
+{
+  // totally trivial example
+  yield "Apple";
+  yield "Ball";
+  yield "Cat";
+}
+
+$stuff = getValues();
+foreach ($stuff as $thing ) {
+  echo $thing . "\n";
+}
